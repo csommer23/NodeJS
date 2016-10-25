bf857e2948652b2e8d691472ee742695c545e0f8
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2016-10-25 08:02:56 -0200

    Example post

---
+++ 8 - post/post.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const http = require('http');
+const queryString = require('querystring');
+
+const postData = queryString.stringify({
+    name: 'Jean Nascimento',
+    type: 'professor'
+});
+
+const options = {
+    host: 'webschool-io.herokuapp.com',
+    method: 'POST',
+    path: '/api/pokemons',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length
+    }
+};
+
+function callback(res) {
+    console.log('STATUS:' + res.statusCode);
+    console.log('HEADERS:' + JSON.stringify(res.headers));
+
+    let data = '';
+
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    res.on('end', () => {
+        console.log('Dados:', data)
+    });
+}
+
+const req = http.request(options, callback);
+
+req.on('error', (e) => {
+    console.log('ERRO: ' + e.message);
+});
+
+req.write(postData);
+req.end();
